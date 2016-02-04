ee5fdb7f9e501464533d6efab9e15702f6a97013
rolpe <ronlipkin14@gmail.com>
2016-02-03 22:49:46 -0500

    Inital Commit

--- Stack-Exercise/ViewController.swift
+++ Stack-Exercise/ViewController.swift
@@ -10,9 +10,17 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var postBtn: UIButton!
+    @IBOutlet weak var favoritesBtn: UIButton!
+    @IBOutlet weak var mailBtn: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        postBtn.imageView?.contentMode = UIViewContentMode.ScaleAspectFit
+        favoritesBtn.imageView?.contentMode = UIViewContentMode.ScaleAspectFit
+        mailBtn.imageView?.contentMode = UIViewContentMode.ScaleAspectFit
+        
     }
 
     override func didReceiveMemoryWarning() {
